66098be3d44b60f05d978393b473cfdffa1da88e
ecoinchTech <ecoinch.digital@gmail.com>
2025-08-06 10:47:54 +0530

    bug fix again in env

--- server.js
+++ server.js
@@ -11,12 +11,25 @@ const PORT = process.env.PORT || 5000
 
 // use both
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://ecoinch.com",
+  "https://www.ecoinch.com",
+]
+
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://ecoinch.com"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
     credentials: true,
   })
 )
+
 
 app.use(express.json())
 
